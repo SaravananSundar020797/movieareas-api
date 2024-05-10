643d54a017d9a2d6f5eb5c00e0e95c933326bf6a
SaravananSundar <saravananmech020797@gmail.com>
2024-05-11 04:12:53 +0530

    Route structure

--- main.js
+++ main.js
@@ -7,6 +7,35 @@ app.get('/',(req,res)=>{
   res.json({msg:"Hellow Students!"})
 })
 
+
+
+// CRUD functionality of movies
+
+// R - For Reading
+
+app.get('./movies',(req,res) => {
+  
+});
+
+// C - For creating movies
+
+app.post('./movies',() => {
+
+});
+
+// U - For update movies
+
+app.put('./movies/:id',() => {
+
+});
+
+// D - For delete movies
+
+app.delete('./movies/:id',() => {
+
+})
+
+
 app.listen(PORT, () => {
   console.log(`The server is running at http://localhost:${PORT}`);
 })
